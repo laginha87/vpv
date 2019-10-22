e313bb5a5ceb8669a83b779e0446dcfe7dd4c27a
Filipe Correia <filipe.lp.correia@gmail.com>
2019-10-22 08:12:01 +0100

    WIP

--- src/components/Home/HomeComponent.tsx
+++ src/components/Home/HomeComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MarkerI, MapComponent } from '../Map/MapComponent';
 
 
@@ -12,12 +12,42 @@ const markers : MarkerI[] = [
   {position: [38.7863488,-9.1588781], type: 'campaign'}
 ]
 
+interface Campaign {
+  name : string;
+  number_of_voluntaries: number;
+  distance: number;
+  percentage_complete: number;
+  end_date: number;
+}
+const campaign = [
+  {
+    name: "Corporação de Vilar",
+    number_of_voluntaries: 5,
+    distance: 25,
+    percentage_complete: 65,
+    end_date: 20,
+  }
+]
+
+const campaigns : Campaign[] = Array(10).fill(campaign);
+
+const CampaignCard = ({campaign: {number_of_voluntaries,name, distance, end_date, percentage_complete}} : {campaign: Campaign}) => (<div className='rounded shadow p-5'>
+  <div className='text-grey-900 font-bold text-xl'>{name}</div>
+  <div className="bg-grey-500">Há {number_of_voluntaries} voluntários envolvidos na campanha</div>
+  <div className="text-grey-300">Estás a {distance} minutos de carro</div>
+  <div className='flex'>
+    <div className='font-bold'>{percentage_complete}% da Campanha Suprida</div>
+  </div>
+
+</div>)
+
+
 export const HomeComponent = (props : HomeProps) => {
   return <div>
     <MapComponent markers={markers as any} />
-    <div className='mh-6 rounded w-full z-10 absolute bottom-0'>
-      <div>Campanhas ativas na sua área</div>
-
+    <div className='p-6 rounded w-full z-10 absolute bg-grey-100' style={{top: "40vh", maxHeight: "60vh", overflow: 'scroll'}}>
+      <div className='text-center text-grey-900 font-bold text-xl mb-4'>Campanhas ativas na tua área</div>
+      {campaigns && campaigns.map((e) => <CampaignCard campaign={e}/>)}
     </div>
   </div>;
 }
