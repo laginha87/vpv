d5006a035f388cbeaa0263e6b86d3a0946902a8a
Filipe Correia <filipe.lp.correia@gmail.com>
2019-10-23 11:38:01 +0100

    Fix up stuff

--- stories/2-Icon.stories.js
+++ stories/2-Icon.stories.js
@@ -4,14 +4,15 @@ import '../src/tailwind.css';
 
 import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs';
-
+import { TEXT_COLORS, BG_COLORS } from '../src/styles/colors';
 const stories = storiesOf('Icon Stories', module);
 
 
 stories.add('as dynamic variables', () => {
   const icons = select('Icon', ICON_NAMES);
-  const colors = select('Icon', COLOR_NAMES);
+  const bgColor = select('Bg Color', BG_COLORS);
+  const textColor = select('Text Color', TEXT_COLORS);
   return <div className='p-6 w-full'>
-  <Icon icon={icons}/>
+  <div className={[bgColor, textColor].join(" ")}><Icon icon={icons}/></div>
 </div>;
 })
